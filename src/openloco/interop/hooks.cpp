187afd21e51f63e51fe634497f1882ad0fe1d09d
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2018-12-12 16:30:38 +0100

    Decompile viewport management

--- src/openloco/interop/hooks.cpp
+++ src/openloco/interop/hooks.cpp
@@ -19,6 +19,7 @@
 #include "../ui.h"
 #include "../ui/WindowManager.h"
 #include "../utility/string.hpp"
+#include "../viewportmgr.h"
 #include "interop.hpp"
 
 using namespace openloco;
@@ -826,6 +827,7 @@ void openloco::interop::register_hooks()
     ui::tooltip::register_hooks();
     ui::vehicle::registerHooks();
     ui::WindowManager::registerHooks();
+    ui::viewportmgr::registerHooks();
 
     register_hook(
         0x004AB655,
